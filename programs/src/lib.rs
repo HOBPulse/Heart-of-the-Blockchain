26e916f205cde6b536768842e79e45735a756019
Mohammed Zeeshan <93642091+alizeeshan1234@users.noreply.github.com>
2025-04-30 03:25:18 +0530

    Create lib.rs

---
+++ programs/src/lib.rs
@@ -0,0 +1,29 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("9FHnJ6S5P1UoWtyd6iqXYESy4WEGvGArA5W17f6H1gQk");
+
+#[program]
+pub mod heart_of_blockchain {
+    use super::*;
+
+    pub fn init_campaign(ctx: Context<InitializeCampaign>, campaign_id: u64, title: String, description: String) -> Result<()> {
+        ctx.accounts.init_campaign(campaign_id, title, description)
+    }
+
+    pub fn init_doner(ctx: Context<InitDoner>, campaign: Pubkey) -> Result<()> {
+        ctx.accounts.init_doner(campaign)
+    }
+
+    pub fn donate_amount(ctx: Context<DonateAmount>, campaign_id: u64, title: String, donation_amount: u64) -> Result<()> {
+        ctx.accounts.donate_amount(campaign_id, title, donation_amount)
+    }
+}
